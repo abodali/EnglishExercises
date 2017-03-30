d81af4d0ba78acea34a65eee94abd496f57c2974
Atanas Yankov <yankov1988@gmail.com>
2017-03-30 11:58:08 +0300

    bug fixed Mazalo no raboti nujda ot mnogo refakturirane

--- app/src/main/java/com/example/nasko/englishexercises/activities/PreExersciseActivity.java
+++ app/src/main/java/com/example/nasko/englishexercises/activities/PreExersciseActivity.java
@@ -7,21 +7,28 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 
 import com.example.nasko.englishexercises.R;
+import com.example.nasko.englishexercises.databases.FeedReaderDbHelper;
 import com.example.nasko.englishexercises.entries.EntryList;
 import com.example.nasko.englishexercises.entries.MyEntity;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class PreExersciseActivity extends AppCompatActivity {
     Button startBtn;
     Spinner spinnerTypes;
     EditText howManyExercises;
     Intent intent;
+    List<MyEntity> myEntityList;
+    Map<String, Integer> mapOfTypesCounter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,6 +37,24 @@ public class PreExersciseActivity extends AppCompatActivity {
         spinnerTypes = (Spinner) findViewById(R.id.spinnerTypes);
         howManyExercises = (EditText) findViewById(R.id.howManyExercises);
         intent = new Intent(this, ExerciseActivity.class);
+
+        myEntityList = new ArrayList<>();
+        mapOfTypesCounter = new HashMap<>();
+
+        fillTheMap();
+
+        spinnerTypes.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                setHint();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                // your code here
+            }
+
+        });
 
 
         startBtn.setOnClickListener(new View.OnClickListener() {
@@ -80,6 +105,41 @@ public class PreExersciseActivity extends AppCompatActivity {
 
             default:
                 return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        fillTheMap();
+        setHint();
+    }
+
+    private void fillTheMap(){
+        this.myEntityList = EntryList.getAllDB(getApplication());
+        mapOfTypesCounter.clear();
+        for (MyEntity entity : myEntityList) {
+            if (!mapOfTypesCounter.containsKey(entity.getType())){
+                mapOfTypesCounter.put(entity.getType(), 0);
+            }else {
+                int count = mapOfTypesCounter.get(entity.getType());
+                count++;
+                mapOfTypesCounter.put(entity.getType(), count);
+            }
+        }
+    }
+
+    private void setHint(){
+        String selectedType = spinnerTypes.getSelectedItem().toString();
+        if (selectedType.equals("All")){
+            howManyExercises.setHint(String.valueOf(myEntityList.size()));
+        } else {
+            if (mapOfTypesCounter.containsKey(selectedType)){
+                int count = mapOfTypesCounter.get(selectedType);
+                howManyExercises.setHint(String.valueOf(count));
+            }else {
+                howManyExercises.setHint("0");
+            }
         }
     }
 }
